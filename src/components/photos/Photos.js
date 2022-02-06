19c4eddc38f3b2b7147017677709fef1961e7e39
JacobTTorres <jacobttorres@hotmail.com>
2022-02-06 14:56:16 -0600

    Photos desktop done

---
+++ src/components/photos/Photos.js
@@ -0,0 +1,19 @@
+import "./photos.css";
+
+import Milk from "../../images/desktop/image-gallery-milkbottles.jpg";
+import Orange from "../../images/desktop/image-gallery-orange.jpg";
+import Cone from "../../images/desktop/image-gallery-cone.jpg";
+import Sugar from "../../images/desktop/image-gallery-sugarcubes.jpg";
+
+export default function Photos() {
+  return (
+    <section className="photosContainer">
+      <div className="photoContent">
+        <img src={Milk} alt="Milk" className="photoImg" />
+        <img src={Orange} alt="Orange" className="photoImg" />
+        <img src={Cone} alt="Cone" className="photoImg" />
+        <img src={Sugar} alt="Sugar" className="photoImg" />
+      </div>
+    </section>
+  );
+}
